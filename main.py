c49651c094018d3fbd3ff4211ebcf5ae9ce79b1e
DmitrijsAstrosaps <125596139+DmitrijsAstrosaps@users.noreply.github.com>
2023-02-17 15:52:37 +0000

    commit main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def find_mismatch(text):
                 return i+1
             if (len(opening_brackets_stack)==0):
                 return -45;
-        if text.find("(e)"):
+        if next.find("(e)"):
             return -9;
         
 
