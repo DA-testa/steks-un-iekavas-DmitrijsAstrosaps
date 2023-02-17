dd3b847fd802dd1c48631ab61f4b83786411151b
DmitrijsAstrosaps <125596139+DmitrijsAstrosaps@users.noreply.github.com>
2023-02-17 16:00:09 +0000

    commit main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def find_mismatch(text):
             x=next.find("(e)")
             if (x==1):
                 return -9;
-                break;
+            break;
         if next in ")]}":
             # Process closing bracket, write your code here
             pers = ")]}".index(next)
